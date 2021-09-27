8572436026871a1789aad29677594bc27b91dc5e
Leon Si <leonzalion@gmail.com>
2021-09-26 21:37:04 -0400

    added comment

--- src/types/modules.ts
+++ src/types/modules.ts
@@ -1,11 +1,11 @@
-/* eslint-disable class-methods-use-this */
-
 import type { UnionToIntersection } from 'utility-types';
 
 import * as lionecsModules from '../modules';
 import type { ComponentBase, ComponentState } from './state';
 
+// https://stackoverflow.com/questions/50321419/typescript-returntype-of-generic-function
 class ModulesWrapper<C extends ComponentBase, S extends ComponentState<C>> {
+	// eslint-disable-next-line class-methods-use-this
 	wrapped() {
 		return lionecsModules['' as keyof typeof lionecsModules]<C, S>();
 	}
