8c3b9bf3ed4b5f22707f43390531073d953b8e9c
Leon Si <contact@leonzalion.com>
2021-12-03 14:33:27 -0500

    update component type

--- src/modules/access/query.ts
+++ src/modules/access/query.ts
@@ -1,22 +1,39 @@
-import type { ComponentFromKey, ComponentKey, ComponentMap } from '~/types/component';
+import type {
+	ComponentFromKey,
+	ComponentKey,
+	ComponentMap,
+} from '~/types/component';
 import { useDefineMethods } from '~/utils/methods';
 
-type Query<M extends ComponentMap> = {
+type ComponentKeyOrComponent<
+	M extends ComponentMap,
+	KS extends ComponentKey<M>[]
+> = {
+	// Can either be an array of component keys or an array
+	// of components
+	[K in keyof KS]: KS[K] | (K extends number ? ComponentFromKey<M, KS[K]> : never)
+};
 
-}
+type Query<M extends ComponentMap, KS extends ComponentKey<M>[]> = {
+	components: ComponentKeyOrComponent<M, KS>;
+};
 
 export function queryModule<M extends ComponentMap>() {
 	const defineMethods = useDefineMethods<M>();
-
 	return defineMethods({
 		/**
 		 * Creates a query that matches entities based on their components. The
 		 * query is lazy and only iterates through the components when another function
 		 * is activated with the query.
 		 */
-		createQuery: function <KS extends ComponentKey<M>[]>(
-			components: KS | { [K in KS[number]]: ComponentFromKey<M, K> }
-		) {
+		createQuery: function <KS extends ComponentKey<M>[]>(query: Query<M, KS>) {
+			const componentKeys = query.components.map((c) =>
+				this.getComponentKey(c)
+			);
+
+			return {
+				componentKeys,
+			};
 			/*
 			let minComponentLen = 0;
 			let minComponentIndex = 0;
