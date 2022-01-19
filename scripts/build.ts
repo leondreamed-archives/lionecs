062bf8c4c3b022fe6ec126f2385c413c6eb8dd38
Leon Si <contact@leonzalion.com>
2022-01-18 20:09:35 -0500

    refactor: use lion-system

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,18 +1,18 @@
+import { rmDist, copyPackageFiles } from 'lion-system';
 import * as process from 'node:process';
+import * as fs from 'node:fs';
 import { execaCommandSync as exec } from 'execa';
-import fs from 'fs-extra';
 
-process.chdir('..');
-
-fs.rmSync('dist', { force: true, recursive: true });
+rmDist();
 exec('tsc');
 exec('tsc-alias -p tsconfig.json');
+copyPackageFiles();
 
 for (const pluginDir of fs.readdirSync('src/plugins')) {
 	const pluginPath = `src/plugins/${pluginDir}`;
 	const pluginDestPath = `dist/plugin/${pluginDir}`;
 
-	fs.mkdirpSync('dist/plugin');
+	fs.mkdirSync('dist/plugin', { recursive: true });
 	exec(`tsc -p ${pluginPath}/tsconfig.json --outDir ${pluginDestPath}`);
 	exec(`tsc-alias -p ${pluginPath}/tsconfig.json`);
 
@@ -25,4 +25,4 @@ for (const pluginDir of fs.readdirSync('src/plugins')) {
 	}
 }
 
-fs.copyFileSync('package.json', 'dist/package.json');
+
