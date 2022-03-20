f0718f9ad443ce8a5dc03c4b4cc4cf5c07112332
Leon Si <contact@leonzalion.com>
2022-03-20 06:36:29 -0400

    chore: prettier

--- test/defs/component.ts
+++ test/defs/component.ts
@@ -1,7 +1,6 @@
 import * as componentDefinitions from './component-defs/index.js';
 import { defsToComponents } from '~/utils/component.js';
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export const Component = defsToComponents(componentDefinitions);
 
 // eslint-disable-next-line @typescript-eslint/no-redeclare
--- test/defs/entity.ts
+++ test/defs/entity.ts
@@ -3,7 +3,6 @@ import { useDefineEntities } from '~/exports.js';
 
 const defineEntities = useDefineEntities<Component>();
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
 const GameEntity = defineEntities({
 	player: [Component.health, Component.inventory, Component.name],
 	enemy: [Component.health, Component.damage],
