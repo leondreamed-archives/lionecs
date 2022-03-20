27c687f780f27a31470e1823c366156ca272c989
Leon Si <contact@leonzalion.com>
2022-03-20 06:33:26 -0400

    chore: update deps

--- src/exports.ts
+++ src/exports.ts
@@ -24,5 +24,10 @@ export type { ComponentStateChangeHandler } from './types/handlers.js';
 export type { Lionecs } from './types/lionecs.js';
 export type { EntityProxy as EntityPProxy } from './types/proxy.js';
 export { useDefineEntities } from './utils/entity.js';
-export { defComponent, defTag, isComponent } from './utils/component.js';
+export {
+	defComponent,
+	defTag,
+	isComponent,
+	defsToComponents,
+} from './utils/component.js';
 export { createLionecs } from './utils/lionecs.js';
